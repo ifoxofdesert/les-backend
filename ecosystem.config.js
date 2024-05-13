ef3a82cbe2d842ac5c988372b891d7a9208de8d2
Comelpho <comelpho@gmail.com>
2024-05-13 09:01:33 +0300

    Вернул способ запуска через файл

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'lesBack',
-      script: 'yarn',
+      script: './server.js',
       cwd: '/home/site/les-backend/',
-      args: 'start',
       instances: 3,
       exec_mode: 'cluster',
       max_memory_restart: '900M',

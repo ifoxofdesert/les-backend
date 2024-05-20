3991595e7bf7f68e2aebea3a184311b9cdc68d7e
Comelpho <comelpho@gmail.com>
2024-05-20 19:26:28 +0300

    test

--- config/plugins.js
+++ config/plugins.js
@@ -13,6 +13,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         host: env('SMTP_HOST'),
         port: 587,
+        secure: false,
         auth: {
           user: env('SMTP_USERNAME'),
           pass: env('SMTP_PASSWORD'),
